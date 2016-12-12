ee8b8dabbe312621d74c30d7e9a02480870195c1
osama <osamana@outlook.com>
2016-12-12 13:51:59 +0200

    configured django cms style addon

--- onasite/mysite/settings.py
+++ onasite/mysite/settings.py
@@ -207,3 +207,5 @@ THUMBNAIL_PROCESSORS = (
     'filer.thumbnail_processors.scale_and_crop_with_subject_location',
     'easy_thumbnails.processors.filters'
 )
+
+DJANGOCMS_STYLE_CHOICES = ['container', 'page-heading']
